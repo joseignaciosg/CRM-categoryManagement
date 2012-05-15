fe8a679f4345dbb3d3d2d2edf78b746529fb6171
José Galindo <joseignaciosg9@gmail.com>
2012-05-14 22:10:22 -0300

    first commit (first bootstrap and codeigniter)

---
+++ application/controllers/pages.php
@@ -0,0 +1,37 @@
+<?php
+
+class Pages extends CI_Controller {
+	
+
+	public function home($page = 'home')
+	{
+		if ( ! file_exists('application/views/pages/'.$page.'.php')){
+			show_404();
+		}
+		
+		$data['title'] = ucfirst($page); // Capitalize the first letter
+// 		$data['todo'] = array('clean house', 'eat lunch', 'call mom');
+		
+		
+		$this->load->view('templates/header', $data);
+		$this->load->view('pages/'.$page, $data);
+		$this->load->view('templates/footer', $data);
+
+	}
+	
+	public function about($page = 'about')
+	{
+		if ( ! file_exists('application/views/pages/'.$page.'.php')){
+			show_404();
+		}
+	
+		$data['title'] = ucfirst($page); // Capitalize the first letter
+	
+		$this->load->view('templates/header', $data);
+		$this->load->view('pages/'.$page, $data);
+		$this->load->view('templates/footer', $data);
+	
+	}
+}
+
+?>
---
+++ application/views/templates/footer.php
@@ -0,0 +1,16 @@
+
+
+      <footer>
+        <p>&copy; Company 2012</p>
+      </footer>
+
+    </div> <!-- /container -->
+
+    <!-- Le javascript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="../../assets/js/bootstrap.js"></script>
+    <script src="../../assets/js/bootstrap.min.js.js"></script>
+
+  </body>
+</html>
